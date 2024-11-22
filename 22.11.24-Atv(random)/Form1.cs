2c438e59f1c88d4517e55d4751b6af1d51e1af02
JoaoGabrielAlmeida <jgnatlna@gmail.com>
2024-11-22 09:22:12 -0300

    Adicionar arquivos de projeto.

---
+++ 22.11.24-Atv(random)/Form1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _22._11._24_Atv_random_
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+            Random random = new Random();
+            int[] numeros = new int[10];
+            for (int i = 0;i < numeros.Length; i++)
+            {
+                numeros[i] = random.Next(0, 100);
+            }
+            Array.Sort(numeros);
+            foreach (int numero in numeros)
+            {
+                listBox1.Items.Add(numero);
+            }
+        }
+    }
+}
